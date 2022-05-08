310c8a7d0fca7ff3378df54bbdf7899bc785c4f3
ynyanchoka <monaryymelda@gmail.com>
2022-05-08 12:32:19 +0300

    add templates

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,25 @@
+import spark.ModelAndView;
+import spark.template.handlebars.HandlebarsTemplateEngine;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static spark.Spark.get;
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        get("/",(request, response) -> {
+            Map<String,Object> model=new HashMap<String, Object>();
+            return new ModelAndView(model,"index.hbs");
+        },new HandlebarsTemplateEngine());
+
+        //form animals
+        get( "/animals/new", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+//            model.put("animals", animals);
+            return new ModelAndView(model, "animal-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
     }
 }
